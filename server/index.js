33ec658d79aefd7b2e6074084109045b9b0ad52d
MathSena <MathSena07@hotmail.com>
2022-03-14 22:34:18 -0300

    Criando e configurando a config.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
+import config from './config.js'
 import server from './server.js'
 import { logger } from './util.js'
 
 // Criando o nosso servidor
-server.listen(3000).on('listening', () => logger.info('Servidor rodando'))
+server
+  .listen(config.port)
+  .on('listening', () =>
+    logger.info(`Servidor rodando na porta ${config.port}!!`)
+  )
