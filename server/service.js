081809ced4a9271ca10ffa3230fcc3938dc12872
MathSena <MathSena07@hotmail.com>
2022-03-15 22:05:45 -0300

    Configurando as rotas

--- server/service.js
+++ server/service.js
@@ -1,20 +1,19 @@
 import fs from 'fs'
-import { join, extname } from 'path'
-import config from './config.js'
 import fsPromises from 'fs/promises'
+
+import config from './config.js'
+import { join, extname } from 'path'
 const {
   dir: { publicDirectory }
 } = config
-
 export class Service {
   createFileStream(filename) {
     return fs.createReadStream(filename)
   }
 
   async getFileInfo(file) {
-    // file = home/index.js
+    // file = home/index.html
     const fullFilePath = join(publicDirectory, file)
-
     //Validação
     await fsPromises.access(fullFilePath)
     const fileType = extname(fullFilePath)
@@ -24,7 +23,7 @@ export class Service {
     }
   }
 
-  async getFileStream() {
+  async getFileStream(file) {
     const { name, type } = await this.getFileInfo(file)
     return {
       stream: this.createFileStream(name),
